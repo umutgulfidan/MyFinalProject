39d707458a4e8397affc45be945547c3afd825da
umutgulfidan <umutgulfidan@gmail.com>
2024-01-31 13:24:36 +0300

    Kodlama.io Yazılım Geliştirici Yetiştirme Kampı (C# + ANGULAR) 12. Gün Sonu

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,9 +1,17 @@
+
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
+using Business.DependencyResolvers.Autofac;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory()).ConfigureContainer<ContainerBuilder>(builder =>
+{
+    builder.RegisterModule(new AutofacBusinessModule());
+});
 
 // Add services to the container.
 
@@ -13,8 +21,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 //IoC
-builder.Services.AddSingleton<IProductService,ProductManager>();
-builder.Services.AddSingleton<IProductDal,EfProductDal>();
+//builder.Services.AddSingleton<IProductService,ProductManager>();
+//builder.Services.AddSingleton<IProductDal,EfProductDal>();
 
 var app = builder.Build();
 
