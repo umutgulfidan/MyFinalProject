e707cb41e8b54066a21cb2f36784792d055e03bf
umutgulfidan <umutgulfidan@gmail.com>
2024-02-04 13:21:35 +0300

    Kodlama.io Yazılım Geliştirici Yetiştirme Kampı (C# + ANGULAR) 13. Gün Ders Sonu

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -12,7 +12,10 @@ namespace Business.Constants
         public static string ProductAdded = "Ürün eklendi";
         public static string ProductNameInvalid = "Ürün ismi geçersiz.";
 
-        public static string MaintenanceTime = "Bakım saati";
-        public static string ProductsListed = "Ürünler listelendi";
+        public static string MaintenanceTime = "Bakım saati.";
+        public static string ProductsListed = "Ürünler listelendi.";
+        public static string ProductCountOfCategoryError = "Bir kategoride 10 ürün olabilir.";
+        public static string ProductNameAlreadyExists = "Bu ürün ismi zaten mevcut.";
+        public static string CategoryLimitExceded = "Kategori limiti aşıldığı için yeni ürün eklenemiyor.";
     }
 }
